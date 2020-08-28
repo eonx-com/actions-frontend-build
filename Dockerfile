afce4615da18ffb8b2976b484ef5c49b67291b8b
Damian Sloane <damian@capacitorlabs.com>
2020-08-28 10:27:24 +1000

    Initial release

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:12
+
+RUN apt-get update;
+RUN apt-get install -y --no-install-recommends \
+        libgtk2.0-0 \
+        libnotify-dev \
+        libgconf-2-4 \
+        libnss3 \
+        libxss1 \
+        libasound2 \
+        xvfb;
+RUN rm -rf /var/lib/apt/lists/*
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eox pipefail
+
+PACKAGE_NAME=$(basename ${INPUT_PATH})
+PACKAGE_FOLDER="${GITHUB_WORKSPACE}/${INPUT_PATH}"
+PACKAGE_CONFIG_FILE=$(echo "${PACKAGE_FOLDER}/package.json" | sed s#//*#/#g)
+
+ENVIRONMENT=${INPUT_ENVIRONMENT}
+ENVIRONMENT_FILE=$(echo "${PACKAGE_FOLDER}/.env.${INPUT_ENVIRONMENT}" | sed s#//*#/#g)
+
+echo
+echo "Validating package folder...";
+if [[ ! -d ${PACKAGE_FOLDER} ]]; then
+  echo "ERROR: Could not locate package folder (${PACKAGE_FOLDER})";
+  exit 1;
+fi
+
+echo "Validating package configuration..."
+if [[ ! -f ${PACKAGE_CONFIG_FILE} ]]; then
+  echo "ERROR: Could not locate package configuration file (${PACKAGE_CONFIG_FILE})";
+  exit 2;
+fi
+
+PACKAGE_SCOPE=$(cat ${PACKAGE_CONFIG_FILE} | jq -r '.name')
+if [[ ${PACKAGE_SCOPE} == "null" || -z "${PACKAGE_SCOPE}" ]]; then
+  echo "ERROR: Could not locate package name in configuration file (${PACKAGE_CONFIG_FILE})";
+  exit 3;
+fi
+
+echo "Loading environment variables..."
+if [[ ! -f ${ENVIRONMENT_FILE} ]]; then
+  echo "ERROR: Could not locate environment variables in project folder (${ENVIRONMENT_FILE})";
+  exit 4;
+fi
+
+echo
+echo "Building: ${PACKAGE_NAME} (${ENVIRONMENT})"
+echo "Environment File: ${ENVIRONMENT_FILE}"
+cat ${ENVIRONMENT_FILE}
+
+cd ${GITHUB_WORKSPACE};
+yarn;
+yarn clean;
+npx lerna exec --scope ${PACKAGE_SCOPE} -- "export \$(egrep -v '^#' ${ENVIRONMENT_FILE} | xargs) && yarn build;"
