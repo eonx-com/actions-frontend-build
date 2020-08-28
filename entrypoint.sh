ac605b8855fae58a50a17d72fcf216b464a41f1c
Damian Sloane <damian@capacitorlabs.com>
2020-08-28 11:16:25 +1000

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -eox pipefail
 
-PACKAGE_NAME=$(basename ${INPUT_PATH})
 PACKAGE_FOLDER="${GITHUB_WORKSPACE}/${INPUT_PATH}"
 PACKAGE_CONFIG_FILE=$(echo "${PACKAGE_FOLDER}/package.json" | sed s#//*#/#g)
 
-ENVIRONMENT=${INPUT_ENVIRONMENT}
 ENVIRONMENT_FILE=$(echo "${PACKAGE_FOLDER}/.env.${INPUT_ENVIRONMENT}" | sed s#//*#/#g)
 
 echo
@@ -21,17 +19,12 @@ if [[ ! -f ${PACKAGE_CONFIG_FILE} ]]; then
   exit 2;
 fi
 
-PACKAGE_SCOPE=$(cat ${PACKAGE_CONFIG_FILE} | jq -r '.name')
-if [[ ${PACKAGE_SCOPE} == "null" || -z "${PACKAGE_SCOPE}" ]]; then
-  echo "ERROR: Could not locate package name in configuration file (${PACKAGE_CONFIG_FILE})";
-  exit 3;
-fi
-
 echo "Loading environment variables..."
 if [[ ! -f ${ENVIRONMENT_FILE} ]]; then
   echo "ERROR: Could not locate environment variables in project folder (${ENVIRONMENT_FILE})";
-  exit 4;
+  exit 3;
 fi
+
 cat ${ENVIRONMENT_FILE}
 export \$(egrep -v '^#' ${ENVIRONMENT_FILE} | xargs)
 
