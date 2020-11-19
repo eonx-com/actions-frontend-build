1f3973b6dc91ed4d043baa798412813595fed611
Damian Sloane <damian@capacitorlabs.com>
2020-11-19 12:49:39 +1100

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,9 +37,6 @@ fi
 if [[ ! -z "${INPUT_NPM_TOKEN}" ]]; then
   echo "Setting NPM token..."
   export NPM_TOKEN="${INPUT_NPM_TOKEN}"
-  npm --version
-  npm cache clean
-  npm login --registry=https://npm.pkg.github.com --scope=@eonx-com
 else
   echo "WARNING: No NPM_TOKEN value was specified- private repository access will not be available"
 fi
