f8ee5dc7f1b8540acfc429800f25abdd07a4a29a
Damian Sloane <damian@capacitorlabs.com>
2020-11-19 12:39:20 +1100

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,16 @@ PACKAGE_FOLDER="${GITHUB_WORKSPACE}/${INPUT_PATH}";
 PACKAGE_CONFIG_FILE=$(echo "${PACKAGE_FOLDER}/package.json" | sed s#//*#/#g)
 ENVIRONMENT_FILE=$(echo "${PACKAGE_FOLDER}/.env.${INPUT_ENVIRONMENT}" | sed s#//*#/#g)
 
-# If an NPM token was set- expose it
-if [[ ! -z "${INPUT_NPM_TOKEN}" ]]; then
-  echo "Setting NPM token..."
-  #export NPM_TOKEN="${INPUT_NPM_TOKEN}"
-else
-  echo "WARNING: No NPM_TOKEN value was specified- private repository access will not be available"
+
+if [[ "${INPUT_ENVIRONMENT}" != "dev" && "${INPUT_ENVIRONMENT}" != "staging" && "${INPUT_ENVIRONMENT}" != "prod" ]]; then
+  echo "ERROR: Invalid environment specified (${INPUT_ENVIRONMENT}). Must be one of 'dev', 'staging', or 'prod'"
+  exit 1;
 fi
 
 echo "Validating package folder...";
 if [[ ! -d ${PACKAGE_FOLDER} ]]; then
   echo "ERROR: Could not locate package folder (${PACKAGE_FOLDER})";
-  exit 1;
+  exit 2;
 fi
 cd "${PACKAGE_FOLDER}" || exit 2;
 
@@ -33,6 +31,14 @@ echo "Loading environment variables...";
 if [[ ! -f ${ENVIRONMENT_FILE} ]]; then
   echo "ERROR: Could not locate environment variables in project folder (${ENVIRONMENT_FILE})";
   exit 4;
+fi
+
+# If an NPM token was set- expose it
+if [[ ! -z "${INPUT_NPM_TOKEN}" ]]; then
+  echo "Setting NPM token..."
+  export NPM_TOKEN="${INPUT_NPM_TOKEN}"
+else
+  echo "WARNING: No NPM_TOKEN value was specified- private repository access will not be available"
 fi
 
 # shellcheck disable=SC2086,SC2046
